bde859e9cb76ec2ec38cc33915e9271b5b51bee2
Ezequiel Brilhante <ezequielrb7@gmail.com>
2022-01-22 09:32:34 -0300

    app de estudo utilizando ngrx para gerenciar os estados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
+import { decrementaContador, IAppState, incrementaContador } from './store/app.state';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngrx-tutorial';
+
+  constructor(private store: Store<{ app: IAppState }>) {
+  }
+
+  counter$ = this.store.select('app').pipe(
+    map(e => e.counter)
+  )
+
+  incrementaContador() {
+    this.store.dispatch(incrementaContador())
+  }
+
+  decrementaContador() {
+    this.store.dispatch(decrementaContador())
+
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DisplayContadorComponent } from './components/display-contador/display-contador.component';
+import { StoreModule } from '@ngrx/store';
+import { appReducer } from './store/app.state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayContadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({
+      app: appReducer
+    }, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
